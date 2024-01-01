cbabfa536edebed0604253530ec1f9541891e690
Mohamedmsf9 <m76549450@gmail.com>
2024-01-01 13:29:08 +0100

    Your descriptive commit message

--- src/Teams.jsx
+++ src/Teams.jsx
@@ -17,31 +17,37 @@ export default function Teams() {
 
   return (
     <div className="teams-container">
-      {/* Header row */}
-      <p>
-        <span>#</span>
-        <span>Team</span>
-        <span>MP</span>
-        <span>W</span>
-        <span>D</span>
-        <span>L</span>
-        <span>GD</span>
-        <span>PTS</span>
-      </p>
-
-      {/* Teams standings */}
-      {teams.map((team, index) => (
-        <p key={index}>
-          <span>{team.overall_league_position}</span>
-          <span><img width={20} src={team.team_badge} alt={team.team_name} /> {team.team_name}</span>
-          <span>{team.overall_league_payed}</span>
-          <span>{team.overall_league_W}</span>
-          <span>{team.overall_league_D}</span>
-          <span>{team.overall_league_L}</span>
-          <span>{team.overall_league_GF - team.overall_league_GA}</span>
-          <span>{team.overall_league_PTS}</span>
-        </p>
-      ))}
+      <table className="teams-table">
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>Team</th>
+            <th>MP</th>
+            <th>W</th>
+            <th>D</th>
+            <th>L</th>
+            <th>GD</th>
+            <th>PTS</th>
+          </tr>
+        </thead>
+        <tbody>
+          {teams.map((team, index) => (
+            <tr key={index}>
+              <td>{team.overall_league_position}</td>
+              <td className="team-info">
+                <img width={20} src={team.team_badge} alt={team.team_name} />
+                <span>{team.team_name}</span>
+              </td>
+              <td>{team.overall_league_payed}</td>
+              <td>{team.overall_league_W}</td>
+              <td>{team.overall_league_D}</td>
+              <td>{team.overall_league_L}</td>
+              <td>{team.overall_league_GF - team.overall_league_GA}</td>
+              <td>{team.overall_league_PTS}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 }
